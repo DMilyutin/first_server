5f92e473c46956d989039c2928ac70468aae59af
D Milyutin <dima.milyutin.1996@gmail.com>
2018-02-19 19:14:01 +0300

    lvl 2

--- src/main/java/com/qoobico/remindme/server/config/WebConfig.java
+++ src/main/java/com/qoobico/remindme/server/config/WebConfig.java
@@ -4,9 +4,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.qoobico.remindme.server")
-public class WebConfig extends WebMvcConfigurationSupport{
+public class WebConfig extends WebMvcConfigurerAdapter{
 }
